4044dda82ab73cdfd7b603ebe87326ccc0fbd297
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2022-05-14 17:48:50 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,31 +1,3 @@
-const $ = mdui.$;
-
-function getParam(reqParam) {
-    reqParam = reqParam.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    const paraReg = new RegExp('[\\?&]' + reqParam + '=([^&#]*)');
-    const results = paraReg.exec(window.location);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
-
-// 踩蛋
-console.log("就欢迎一下下你来到本孤寡1IP小站吧！嘿嘿嘿")
-console.log("\n %c Fmkli的咕咕主页 %c By Fmkli\n\n","color: #fff; background: #21AAF8; padding:5px 0;","background: #21AAF8; padding:5px 0;");
-
-// 改变顶部导航栏的颜色
-const getScrollPosition = (el = window) => ({
-    x: el.pageXOffset !== undefined ? el.pageXOffset : el.scrollLeft,
-    y: el.pageYOffset !== undefined ? el.pageYOffset : el.scrollTop
-});
-
-function changeNavColor() {
-    // console.log(getScrollPosition());
-    if (getScrollPosition().y >= window.innerHeight * 0.3) {
-        $("body").addClass("mdui-theme-primary-teal")
-    } else {
-        $("body").removeClass("mdui-theme-primary-teal")
-    }
-}
-window.onscroll = changeNavColor;
 
 // 获取近期文章
 document.onreadystatechange = function () {
@@ -35,7 +7,7 @@ document.onreadystatechange = function () {
         /* Posts */
         $.ajax({
             method: 'GET',
-            url: 'https://api.rss2json.com/v1/api.json?rss_url=https://blog.imfmkli.top/feed/',
+            url: 'https://api.rss2json.com/v1/api.json?rss_url=https://blog.imfmkli.top/atom.xml',
             // url: 'https://blog.fmkli.ga/search.json',
             success: function (data) {
                 var data = JSON.parse(data).items;
@@ -45,7 +17,7 @@ document.onreadystatechange = function () {
                     var element = document.createElement('a');
                     element.href = data[i].link;
                     // element.href = "https://blog.imfmkli.top" + data[i].url;
-                    element.classList = "mdui-list-item mdui-ripple";
+                    element.classList = "gd-box gd-suit gd-rc gd-shadow-20 gd-shadow-h-5";
                     element.textContent = data[i].title;
                     element.target = "blank"
                     container.appendChild(element);
@@ -58,90 +30,3 @@ document.onreadystatechange = function () {
     }
 }
 
-
-var support = new mdui.Dialog('#support');
-
-function donate() {
-    support.open();
-}
-
-switch (getParam('ref')) {
-    case 'donate':
-        donate();
-        break;
-    default:
-        break;
-}
-function getWeather() {
-    if (getParam("city") !== "") {
-        $.ajax({
-            method: 'GET',
-            url: 'https://api.cyfan.top/weather?location=' + getParam("city"),
-            success: function (d) {
-                var d = JSON.parse(d);
-                var weatherDay = d.weather[0].info.day;
-                var weatherNight = d.weather[0].info.night;
-                document.getElementById("day-weather").innerHTML = `<h3 class="mdui-text-center">${weatherDay[1]}, ${weatherDay[2]}°C, ${weatherDay[4]}（${weatherDay[3]}）</h3>
-                `
-                document.getElementById("night-weather").innerHTML = `<h3 class="mdui-text-center">${weatherNight[1]}, ${weatherNight[2]}°C, ${weatherNight[4]}（${weatherNight[3]}）</h3>
-                `
-                console.log(weatherDay, weatherNight);
-                $("#weather-spinner").remove();
-            }
-        });
-    } else {
-        $.ajax({
-            method: 'GET',
-            url: 'https://api.myip.la/cn?json',
-            success: function (data) {
-                var data = JSON.parse(data);
-                $.ajax({
-                    method: 'GET',
-                    url: 'https://api.cyfan.top/weather?location=' + data.location.city,
-                    success: function (d) {
-                        var d = JSON.parse(d);
-                        var weatherDay = d.weather[0].info.day;
-                        var weatherNight = d.weather[0].info.night;
-                        document.getElementById("day-weather").innerHTML = `<h3 class="mdui-text-center">${weatherDay[1]}, ${weatherDay[2]}°C, ${weatherDay[4]}（${weatherDay[3]}）</h3>
-                        `
-                        document.getElementById("night-weather").innerHTML = `<h3 class="mdui-text-center">${weatherNight[1]}, ${weatherNight[2]}°C, ${weatherNight[4]}（${weatherNight[3]}）</h3>
-                        `
-                        console.log(weatherDay, weatherNight);
-                        $("#weather-spinner").remove();
-                    }
-                });
-
-            }
-        });
-    }
-
-}
-
-if (document.referrer) {
-    console.log(document.referrer);
-
-
-    console.log(
-        '%c请注意！我们不会收集并发送上述的来源网址信息！',
-        'color: red'
-    );
-}
-
-
-// 获取原始的页面标题
-var normal_title = document.title;
-// 监听页面的可见性
-document.addEventListener('visibilitychange', function () {
-    if (document.visibilityState == 'hidden') {
-        // 如果页面不可见
-        // 设定新标题
-        document.title = '大爷来玩啊[doge]';
-    } else {
-        // 欢迎信息
-        document.title = "欢迎回来_(:зゝ∠)_ - Fmkli的站点";
-        // 1 秒后恢复原始信息
-        setTimeout(function () {
-            document.title = normal_title
-        }, 1000)
-    }
-});
