4ef70a531bbb04d7ae999972da31d2b1f6e51334
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2022-05-21 15:59:40 +0800

    Update wel.js

--- wel.js
+++ wel.js
@@ -8,3 +8,19 @@ var reful=document.referrer;
 	        console.log("欢迎来自 "+reful+" 的小可爱光临本站！");
 	    };
     }
+    function start(){
+        let str = '<h2>欢迎来到</h2><h1>屑鸽子Fmkliの主页</h1>'
+        let str_ = ''
+        let i = 0
+        let content = document.getElementById('contentss')
+        let timer = setInterval(()=>{
+            if(str_.length<str.length){
+                str_ += str[i++]
+                content.innerHTML = '<p>'+str_+'</p>'                        //打印时加光标
+            }else{ 
+                clearInterval(timer)
+                content.innerHTML = '<p>'+str_+'</p>'
+            }
+        },100)
+    }
+    start()
