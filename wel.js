6e5c85e2ed5c676a4dafedd3455fc436221bd629
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2022-05-21 13:43:12 +0800

    Update wel.js

--- wel.js
+++ wel.js
@@ -4,7 +4,7 @@ var reful=document.referrer;
             document.getElementById("welcome").innerHTML = "欢迎小可爱光临本站！";
 		    console.log("欢迎小可爱光临本站！");
 	    } else {
-            document.getElementById("welcome").innerHTML = "欢迎来自"+reful+"的小可爱光临本站！";
-	        console.log("欢迎来自"+reful+"的小可爱光临本站！");
+            document.getElementById("welcome").innerHTML = "欢迎来自 "+reful+" 的小可爱光临本站！";
+	        console.log("欢迎来自 "+reful+" 的小可爱光临本站！");
 	    };
     }
