16e4e5a7a98854af99d74df6d6cd820d6bf48778
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2022-06-25 05:58:37 +0000

    fdg

--- wel.js
+++ wel.js
@@ -1,10 +1,8 @@
 var reful=document.referrer;
     function test(){
         if (!reful){
-            document.getElementById("welcome").innerHTML = "欢迎小可爱光临本站！";
 		    console.log("欢迎小可爱光临本站！");
         } else {
-            document.getElementById("welcome").innerHTML = "欢迎来自 "+reful+" 的小可爱光临本站！";
 	        console.log("欢迎来自 "+reful+" 的小可爱光临本站！");
 	    };
     }
