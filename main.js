f3534715b9fad6bf87fceecc33178bb456cf9c7d
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2022-07-18 16:08:53 +0800

    dtgfh

--- main.js
+++ main.js
@@ -178,3 +178,12 @@ function gettip(){
     	alert("你 被 骗 了");
     }
 }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then((res) => console.log('service worker registered'))
+            .catch((err) => console.log('service worker not registered', err));
+    });
+}
---
+++ sw.js
@@ -0,0 +1,24 @@
+const staticDevCoffee = '鸽子主页 | Fmkli的鸽子窝';
+const assets = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/app.js',
+    '/images/coffee1.jpg',
+    '/images/coffee2.jpg',
+    '/images/coffee3.jpg',
+    '/images/coffee4.jpg',
+    '/images/coffee5.jpg',
+    '/images/coffee6.jpg',
+    '/images/coffee7.jpg',
+    '/images/coffee8.jpg',
+    '/images/coffee9.jpg',
+];
+
+self.addEventListener('fetch', (fetchEvent) => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then((res) => {
+            return res || fetch(fetchEvent.request);
+        })
+    );
+});
