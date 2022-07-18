cd3159d22d5076a2f3f4c40071ae0506af67f731
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2022-07-18 16:17:45 +0800

    fsgh

--- main.js
+++ main.js
@@ -183,7 +183,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
         navigator.serviceWorker
             .register('/sw.js')
-            .then((res) => console.log('service worker registered'))
-            .catch((err) => console.log('service worker not registered', err));
+            .then((res) => console.log('Service Worker注册成功啦！'))
+            .catch((err) => console.log('Service Worker注册失败了（', err));
     });
 }
