35e358825639824e7b77ca12a20fcc4aca3b3e77
fmkli <fmkli2@qq.comgit config --global user.email fmkli2@qq.com>
2022-07-24 07:12:40 +0000

    fg

--- main.js
+++ main.js
@@ -100,11 +100,11 @@ $.ajax({
         var data = JSON.parse(data).items;
         var list2333 = document.getElementById("posts-list");
         for (var i = 0; i <= 4; i++){
-
             var links = data[i].link;
             var testContent2333 = data[i].title;
             var div2333 = document.createElement('div')
             div2333.classList = "posts2333";
+            div2333.id = "postid" + i
             div2333.innerHTML = '<a href="' + links + '">' + testContent2333 + "</a>"
             list2333.appendChild(div2333);
         }
