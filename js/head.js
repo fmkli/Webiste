835ecd8d8682509a8df68631c21e0f618fa5fc62
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2022-08-11 11:26:01 +0800

    Add files via upload

--- js/head.js
+++ js/head.js
@@ -1,7 +1,7 @@
 console.log('begin')
 pagehieght = document.documentElement.clientHeight
 pagewidth = document.documentElement.clientWidth
-document.write('<div id="loadingpage" class="dailong" style="text-align: center;"><h2>页面加载中，等等哒~</h2></div>')
+document.write('<div id="loadingpage" class="dailong" style="text-align: center;"><div style="top: 30%;position: relative;"><h1>页面加载中，等等哒~</h1></div></div>')
 document.onreadystatechange = jiazaiwancheng;
 function jiazaiwancheng(){
     if(document.readyState == 'complete'){
