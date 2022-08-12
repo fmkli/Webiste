38521f416af83f34d00c765c4d9e5780a0510ec7
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2022-08-12 14:25:01 +0800

    Add files via upload

--- js/head.js
+++ js/head.js
@@ -9,3 +9,11 @@ function jiazaiwancheng(){
         document.getElementById('loadingpage').parentNode.removeChild(document.getElementById('loadingpage'))
     }
 }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker
+            .register('./sw.js')
+            .then((res) => console.log('Service Worker注册成功啦！'))
+            .catch((err) => console.log('Service Worker注册失败了（', err));
+    });
+}
