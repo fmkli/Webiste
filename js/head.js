4fa2e976362c8d47840d4459a0e7b6b9622f363e
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2022-08-13 15:31:35 +0800

    Add files via upload

--- js/head.js
+++ js/head.js
@@ -1,14 +1,4 @@
 console.log('begin')
-pagehieght = document.documentElement.clientHeight
-pagewidth = document.documentElement.clientWidth
-document.write('<div id="loadingpage" class="dailong" style="text-align: center;"><div style="top: 30%;position: relative;"><h1>页面加载中，等等哒~</h1></div></div>')
-document.onreadystatechange = jiazaiwancheng;
-function jiazaiwancheng(){
-    if(document.readyState == 'complete'){
-        console.log('加载完成')
-        document.getElementById('loadingpage').parentNode.removeChild(document.getElementById('loadingpage'))
-    }
-}
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
         navigator.serviceWorker
