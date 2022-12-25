2a56cc0a8417fe55c9b4d41fa0529dc4d50b313c
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2022-12-25 18:40:19 +0800

    114

--- js/head.js
+++
@@ -1,9 +0,0 @@
-console.log('begin')
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker
-            .register('./sw.js')
-            .then((res) => console.log('Service Worker注册成功啦！'))
-            .catch((err) => console.log('Service Worker注册失败了（', err));
-    });
-}
