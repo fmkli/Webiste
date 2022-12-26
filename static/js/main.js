ebc90d38a952ca9f59b42bdf1c0a0eafa624f810
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2022-12-26 16:29:48 +0800

    114

--- static/js/main.js
+++ static/js/main.js
@@ -46,7 +46,7 @@ function Init(feedPath, bgArr, senArr) {
         }
     }
     function articleOutput(){
-        document.getElementById('shuoshuo').innerHTML = '<p class="article-item"><p class="article-title">' + title + '</p></p><br><p class="article-title">' + content2 + '</p>';
+        document.getElementById('shuoshuo').innerHTML = '<p class="article-item"><p class="article-title">' + title + '</p></p><br><p>' + content2 + '</p>';
         console.log('done3')
     }
     console.log('loaddone');
