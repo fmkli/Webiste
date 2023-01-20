2dc98657e9c44aa94eef71b878c6aba46b08152f
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2023-01-20 12:36:35 -0300

    Add files via upload

--- static/js/jiance.js
+++ static/js/jiance.js
@@ -1,3 +1,4 @@
+
 test0 = 1
 test1233 = window.location.search;
 xinxi1 = test1233.split("test=")
@@ -10,11 +11,12 @@ if (xinxi2 = "open"){
     window.addEventListener('error',  args => {
         if (test0 == 1){
             document.body.innerHTML = ""
-            document.write('<div style="text-align: center;color: red;"><h1>JS出现错误</h1><p>请联系Fmkli反馈<br>QQ：2058264685，Email：fmkli2@qq.com<br><br>如果出现 undefined 则可能为资源加载错误</p><hr></div>')
+            document.write('<div style="text-align: center;color: red;"><h1>JS出现错误</h1><p>请联系Fmkli反馈<br>QQ：2058264685，Email：fmkli2@qq.com<br><br>如果出现 undefined 则可能为资源加载错误而不是JS错误，请打开控制台进行进一步查询</p><hr></div>')
         }
         console.log('6')
+        xinxi3 = JSON.stringify(args)
         console.log('error--------------------------event:', args)
-        document.write('<div style="text-align: center;color: red;">文件：' + args.filename+ '<br>错误代码所在行：' + args.lineno + '<br>报错信息：' + args.message + '<hr></div>')
+        document.write('<div style="text-align: center;color: red;">文件：' + args.filename+ '<br>错误代码所在行：' + args.lineno + '<br>报错信息：' + args.message + '<br><hr></div>')
         test0 = test0 + 1
     
         return true
