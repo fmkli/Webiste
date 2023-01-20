c805dc776fec5551e76ccea41c4ca7d42c54426c
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2023-01-20 11:40:52 -0300

    Add files via upload

---
+++ static/js/jiance.js
@@ -0,0 +1,21 @@
+test0 = 1
+test1233 = window.location.search;
+xinxi1 = test1233.split("test=")
+xinxi2 = xinxi1[1].substr(0,4) 
+if (xinxi2 = "open"){
+    console.log("%c网页内部开启了调试模式", "font-size:20px;color:#f44336;")
+}
+
+
+    window.addEventListener('error',  args => {
+        if (test0 == 1){
+            document.body.innerHTML = ""
+            document.write('<div style="text-align: center;color: red;"><h1>JS出现错误</h1><p>请联系Fmkli反馈<br>QQ：2058264685，Email：fmkli2@qq.com<br><br>如果出现 undefined 则可能为资源加载错误</p><hr></div>')
+        }
+        console.log('6')
+        console.log('error--------------------------event:', args)
+        document.write('<div style="text-align: center;color: red;">文件：' + args.filename+ '<br>错误代码所在行：' + args.lineno + '<br>报错信息：' + args.message + '<hr></div>')
+        test0 = test0 + 1
+    
+        return true
+    }, true)
--- static/js/qita.js
+++ static/js/qita.js
@@ -20,51 +20,54 @@ var date = {
 var now = new Date();
 		
 function createtime() {
-    var grt = new Date("08/26/2020 12:00:00"); //此处修改你的建站时间或者网站上线时间 
-    now.setTime(now.getTime() + 250);
-    days = (now - grt) / 1000 / 60 / 60 / 24;
-    dnum = Math.floor(days);
-    hours = (now - grt) / 1000 / 60 / 60 - (24 * dnum);
-    hnum = Math.floor(hours);
-    if (String(hnum).length == 1) {
-        hnum = "0" + hnum;
-    }
-    minutes = (now - grt) / 1000 / 60 - (24 * 60 * dnum) - (60 * hnum);
-    mnum = Math.floor(minutes);
-    if (String(mnum).length == 1) {
-        mnum = "0" + mnum;
-    }
-    seconds = (now - grt) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
-    snum = Math.round(seconds);
-    if (String(snum).length == 1) {
-        snum = "0" + snum;
-    }
-    document.getElementById("timeDate").innerHTML = " 本站已经挣扎度过了 " + dnum + " 天 ";
-    document.getElementById("times").innerHTML = hnum + " 小时 " + mnum + " 分 " + snum + " 秒";
-    // 倒计时截止时间
-	var EndTime = new Date('2023/08/26 12:00:00');
-	// 现在的事件
-	var NowTime = new Date();
-	// 时间差（时间单位：ms）
-	var time = EndTime.getTime() - NowTime.getTime();
-	var day = 0;
-    var hour = 0;
-    var minutes2 = 0;
-    var seconds2 = 0;
-    if(time >= 0) {
-        // 向下取整
-        day = Math.floor(time / 1000 / 60 / 60 / 24);
-        hour = Math.floor(time / 1000 / 60 / 60 % 24);
-        minutes2 = Math.floor(time / 1000 / 60 % 60);
-        seconds2 = Math.floor(time / 1000 % 60);
-    }
-    // 如果是一位数，前面拼接"0"
-    function toDouble(num2) {
-        return num2 < 10 ? '0' + num2 : num2;
-    }
-    
-    document.getElementById("lasttime").innerHTML = "距离网站三周年庆还有" + day + "天 " + hour + "小时 " + minutes2 + "分钟 " + seconds2 + "秒" 
+    if (xinxi2 = "open"){
 
+    } else {
+        var grt = new Date("08/26/2020 12:00:00"); //此处修改你的建站时间或者网站上线时间 
+        now.setTime(now.getTime() + 250);
+        days = (now - grt) / 1000 / 60 / 60 / 24;
+        dnum = Math.floor(days);
+        hours = (now - grt) / 1000 / 60 / 60 - (24 * dnum);
+        hnum = Math.floor(hours);
+        if (String(hnum).length == 1) {
+            hnum = "0" + hnum;
+        }
+        minutes = (now - grt) / 1000 / 60 - (24 * 60 * dnum) - (60 * hnum);
+        mnum = Math.floor(minutes);
+        if (String(mnum).length == 1) {
+            mnum = "0" + mnum;
+        }
+        seconds = (now - grt) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
+        snum = Math.round(seconds);
+        if (String(snum).length == 1) {
+            snum = "0" + snum;
+        }
+        document.getElementById("timeDate").innerHTML = " 本站已经挣扎度过了 " + dnum + " 天 ";
+        document.getElementById("times").innerHTML = hnum + " 小时 " + mnum + " 分 " + snum + " 秒";
+        // 倒计时截止时间
+        var EndTime = new Date('2023/08/26 12:00:00');
+        // 现在的事件
+        var NowTime = new Date();
+        // 时间差（时间单位：ms）
+        var time = EndTime.getTime() - NowTime.getTime();
+        var day = 0;
+        var hour = 0;
+        var minutes2 = 0;
+        var seconds2 = 0;
+        if(time >= 0) {
+            // 向下取整
+            day = Math.floor(time / 1000 / 60 / 60 / 24);
+            hour = Math.floor(time / 1000 / 60 / 60 % 24);
+            minutes2 = Math.floor(time / 1000 / 60 % 60);
+            seconds2 = Math.floor(time / 1000 % 60);
+        }
+        // 如果是一位数，前面拼接"0"
+        function toDouble(num2) {
+            return num2 < 10 ? '0' + num2 : num2;
+        }
+        
+        document.getElementById("lasttime").innerHTML = "距离网站三周年庆还有" + day + "天 " + hour + "小时 " + minutes2 + "分钟 " + seconds2 + "秒" 
+    }
     
 }
 setInterval("createtime()", 250);
@@ -113,4 +116,3 @@ if (date.isDuringDate('2023/01/1', '2023/02/01')){
         position: 'top'
       });
 }
-
