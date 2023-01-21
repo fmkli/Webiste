ab00e1a35f558d23008d1663dfa1ff6f2eff4219
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2023-01-21 01:04:07 -0300

    Add files via upload

--- static/js/jiance.js
+++ static/js/jiance.js
@@ -1,15 +1,11 @@
 
 test0 = 1
 test1233 = window.location.search;
-if (test1233 != ''){
 xinxi1 = test1233.split("test=")
 xinxi2 = xinxi1[1].substr(0,4) 
-}
+
 if (xinxi2 = "open"){
     console.log("%c网页内部开启了调试模式", "font-size:20px;color:#f44336;")
-}
-
-
     window.addEventListener('error',  args => {
         if (test0 == 1){
             document.body.innerHTML = ""
@@ -23,3 +19,4 @@ if (xinxi2 = "open"){
     
         return true
     }, true)
+}
