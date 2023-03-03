ce1c6d3e2ceca17f52218bbe8082093eefee132b
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2023-03-03 10:23:02 -0300

    114

--- static/js/index.js
+++ static/js/index.js
@@ -54,6 +54,8 @@ createtime();
 !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JgapU3Sla12DMquS",ck:"JgapU3Sla12DMquS"});
 console.log('统计开启~')
 
-function coloregg(){
-    alert('你好，世界，你好，生活，一切都会变好。')
-}
+/*
+Not coloregg?
+既然都藏在这了，那就祝福一句吧
+You will be better! I will be better! Everyone will be better! 
+*/
