92a54acafea874e07ff93a9e611d3481b2dbdf96
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2023-04-30 12:58:02 +0000

    1234565

--- static/js/index.js
+++ static/js/index.js
@@ -53,3 +53,17 @@ createtime();
 // 统计统计
 !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JgapU3Sla12DMquS",ck:"JgapU3Sla12DMquS"});
 console.log('统计开启~')
+
+ab = 1
+document.getElementById("footer").style.display = "none"
+function openfooter(){
+    if (ab == 1){
+        document.getElementById("footer").style.display = ""
+        document.getElementById("openfooter").style.top = "70%"
+        ab = 2
+    }else if (ab == 2){
+        document.getElementById("footer").style.display = "none"
+        document.getElementById("openfooter").style.top = "95%"
+        ab = 1
+    }
+}
