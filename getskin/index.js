38102926602a01cb11b50cce755723e92b39fb80
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2023-08-26 19:09:46 +0000

    更新正版玩家工具 - 添加接口

--- getskin/index.js
+++ getskin/index.js
@@ -1,3 +1,4 @@
+//一坨屎山
 function checkjava(){
     console.log('开始检测 - 1')
 document.getElementById('check').innerText = "检测中"
@@ -11,14 +12,28 @@ $.ajax({
 	success: function(data1){
 		document.getElementById('check').innerText = "正版玩家，名字" + data1.data.player.username
         console.log("1")
-        document.getElementById('zbuse').innerHTML = '皮肤头像<br><img src="' + data1.data.player.avatar + '"><br>UUID：' + data1.data.player.id
+        document.getElementById('zbuse').innerHTML = '皮肤头像<br><img src="' + data1.data.player.avatar + '"><br><img src="https://crafatar.com/renders/body/' + data1.data.player.id + '"><br>UUID：' + data1.data.player.id
+        document.getElementById('skin').innerHTML= '<a href="https://crafatar.com/skins/' + data1.data.player.id + '">皮肤下载点这！</a>'
+console.log('2')
+$.ajax({
+	type: "get",
+	url: 'https://crafatar.com/capes/' + data1.data.player.id,
+	success: function(data2){
+		document.getElementById('cape').innerHtml = '<a href="https://crafatar.com/capes/'  + data1.data.player.id + '">此玩家有披风，点我查看</a>'
 console.log('2')
 
+	},
+    error : function(){
+        document.getElementById('cape').innerText = "玩家无披风（接口提示404）"
+
+    }
+});
 	},
     error : function(){
         document.getElementById('check').innerText = "错误或者不存在"
 
     }
 });
+
 
 }
